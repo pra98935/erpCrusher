a7600d2ccee3dd51fb3e23133cee40d4cb0bc901
pra98935 <pra98935@gmail.com>
2020-08-30 01:59:49 +0530

    item master add

--- projects/sales/src/app/master/item-master/im-add/im-add.component.ts
+++ projects/sales/src/app/master/item-master/im-add/im-add.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AddItemMasterModel, commonModel} from '../../../models/item-master.model';
+
 
 @Component({
   selector: 'app-im-add',
@@ -6,10 +8,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./im-add.component.scss']
 })
 export class ImAddComponent implements OnInit {
-
+  addItemMasterModelObj: AddItemMasterModel = new AddItemMasterModel();
+  commonModelObj: commonModel = new commonModel();
+  
   constructor() { }
 
   ngOnInit(): void {
+    this.commonModelObj.ownState='mp';
+    this.addItemMasterModelObj.selectedStateName='mp';
+    
+    this.itemGroupList();
+    this.stateList();
+    this.taxList();
+  }
+
+  itemGroupList(){
+    this.addItemMasterModelObj.itemGroupArray = [
+        {label: 'New York', value: 'NY'},
+        {label: 'Rome', value: 'RM'},
+        {label: 'London', value: 'LDN'},
+        {label: 'Istanbul', value: 'IST'},
+        {label: 'Paris', value: 'PRS'}
+    ];
+  }
+
+  stateList(){
+    this.addItemMasterModelObj.stateNameArray = [
+        {label: 'MP', value: 'mp'},
+        {label: 'AP', value: 'ap'},
+        {label: 'HP', value: 'hp'},
+    ];
+  }
+
+  taxList(){
+    this.addItemMasterModelObj.ownStateTaxArray=[
+      {
+        label: 'CGST', 
+        value: 'cgst'
+      },
+      {
+        label: 'SGST', 
+        value: 'sgst'
+      }
+    ]
+
+    this.addItemMasterModelObj.otherStateTaxArray=[
+      {
+        label: 'IGST', 
+        value: 'igst'
+      }
+    ]
   }
 
 }
---
+++ projects/sales/src/app/models/item-master.model.ts
@@ -0,0 +1,19 @@
+export class AddItemMasterModel{
+    itemName: string;
+    itemGroupArray = [];
+    selectedItemGroup: string;
+    itemShortName: string;
+    status: string;
+    hsnCode: string;
+    stateNameArray = []
+    selectedStateName: string;
+    ownStateTaxArray=[];
+    selectedOwnStateTax: string;
+    otherStateTaxArray=[];
+    selectedOtherStateTax: string;
+
+}
+
+export class commonModel{
+    ownState: string;
+}
